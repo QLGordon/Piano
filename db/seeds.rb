51449153982ad4dac5c5ec7d84e3be63fdcde7bf
Quentin GOURDON <quentin.gourdon@gmail.com>
2018-12-01 18:53:45 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p 'Cleaning Artists'
+Artist.destroy_all
+Instrument.destroy_all
+Event.destroy_all
+
+p 'creating Artists'
+Artist.create({
+  first_name: 'Célimène',
+  last_name: 'Daudet',
+  birth: '21/12/1990',
+  bio: "Célimène est un prénom qui évoque un personnage du Misanthrope de Molière
+Daudet...comme Alphonse?
+Oui! Car il est bien l'arrière-arrière-grand-oncle de Célimène Daudet.
+Avec un nom et un prénom aux lointaines références littéraires,
+Célimène a pourtant fait le choix de la musique!",
+
+})
