ac35c42d5b2f982e0e7b69b35e2bebf7f80de075
Quentin GOURDON <quentin.gourdon@gmail.com>
2018-12-01 20:34:09 +0100

    Add Photo to Artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 p 'Cleaning Artists'
 Artist.destroy_all
 Instrument.destroy_all
@@ -20,5 +15,14 @@ Daudet...comme Alphonse?
 Oui! Car il est bien l'arrière-arrière-grand-oncle de Célimène Daudet.
 Avec un nom et un prénom aux lointaines références littéraires,
 Célimène a pourtant fait le choix de la musique!",
-
 })
+
+100.times do
+  artist = Artist.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    birth: Faker::Date.birthday(18, 65),
+    bio: Faker::Lorem.paragraph(20),
+    photo: Faker::Avatar.image,
+  )
+end
