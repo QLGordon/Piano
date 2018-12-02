f2a5941fa4faec16d23347632fe50a467c7f2aa1
Quentin GOURDON <quentin.gourdon@gmail.com>
2018-12-02 22:12:17 +0100

    instruments seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,12 @@ Event.destroy_all
     photo: 'http://lorempicsum.com/futurama/255/200/' + rand(1..10).to_s,
   )
 end
+
+10.times do
+  offset = rand(Artist.count)
+  rand_record = Artist.offset(offset).first
+  talent = Instrument.create!(
+    instrument: Faker::Music.instrument,
+    artist_id: rand_record.id,
+    )
+end
